c3a71c62531a539bcf56b8fc63e09f1138b3f5c6
Adam Mansur <mansura@si.edu>
2024-08-09 12:52:05 -0400

    Added tests for file utilities

--- nmnh_ms_tools/utils/files.py
+++ nmnh_ms_tools/utils/files.py
@@ -1,17 +1,12 @@
-"""Contains methods to hash a file or image data from a file"""
-
-from __future__ import unicode_literals
+"""Contains methods to hash files and image data"""
 
 import hashlib
 import json
 import logging
 import os
-import shutil
-import subprocess
 from collections import namedtuple
 from pathlib import Path
 
-import yaml
 from PIL import Image
 
 
@@ -54,15 +49,15 @@ class HashCheck:
     def __iter__(self):
         return iter(self._filepaths)
 
-    def hashes(self):
-        return self._hashes
-
     @property
     def index(self):
         index = {}
         for path, idx in self._indexes.items():
             index.update({path.parent / k: v for k, v in idx.items()})
         return index
+
+    def hashes(self):
+        return self._hashes
 
     def hash_file(self, path, overwrite=True):
         path = Path(path).resolve()
@@ -139,7 +134,7 @@ def hasher(filestream, size=8192):
 
     Args:
         filestream (file): stream of file to hash
-        size (int): size of block. Should be multiple of 128.
+        size (int): size of block. Must be multiple of 128.
 
     Return:
         MD5 hash of file
@@ -168,22 +163,7 @@ def hash_file(path):
         return hasher(f)
 
 
-def hash_file_if_exists(path):
-    """Returns MD5 hash of a file, returning None if file doesn't exist
-
-    Args:
-        path (str): path to image
-
-    Returns:
-        Hash as string
-    """
-    try:
-        return hasher(open(path, "rb"))
-    except IOError:
-        return None
-
-
-def hash_image_data(path, output_dir="images"):
+def hash_image_data(path):
     """Returns MD5 hash of the image data in a file
 
     Args:
@@ -192,56 +172,55 @@ def hash_image_data(path, output_dir="images"):
     Returns:
         Hash of image data as string
     """
-    path = os.path.abspath(path)
-    try:
-        return hashlib.md5(Image.open(path).tobytes()).hexdigest()
-    except IOError:
-        # Encountered a file format that PIL can't handle. Convert
-        # file to something usable, hash, then delete the derivative.
-        # The derivatives can be compared to ensure that the image hasn't
-        # been messed up. Requires ImageMagick.
-        fn = os.path.basename(path)
-        jpeg = os.path.splitext(fn)[0] + "_temp.jpg"
-        cmd = 'magick "{}" "{}"'.format(path, jpeg)
-        return_code = subprocess.call(cmd, cwd=output_dir)
-        if return_code:
-            raise IOError("Hash failed: {}".format(fn))
-        dst = os.path.join(output_dir, jpeg)
-        hexhash = hashlib.md5(Image.open(dst).tobytes()).hexdigest()
-        os.remove(dst)
-        return hexhash
+    return hashlib.md5(Image.open(os.path.abspath(path)).tobytes()).hexdigest()
 
 
-def fnv1a_64(data):
-    """Alternative FNV hash algorithm used in FNV-1a
+def fnv1a_64(val, encoding=None):
+    """Hashes value according to alternative FNV hash algorithm used in FNV-1a
 
-    Adapted from https://github.com/znerol/py-fnvhash
+    Adapted from https://github.com/znerol/py-fnvhash.
+
+    Args:
+        val (bytes): the value to hash. If a string is given, the function will
+            try to coerce it to bytes.
+        encoding (bool): the encoding to use when decoding a str. Required if val
+            is a string containing non-ASCII characters.
+
+    Returns:
+        Hash as hex string
     """
+    if isinstance(val, str):
+        val = bytes(val, "ascii") if val.isascii() else bytes(val, encoding=encoding)
+
     hval_init = 0xCBF29CE484222325
     fnv_prime = 0x100000001B3
     fnv_size = 2**64
 
     hval = hval_init
-    for byte in data:
+    for byte in val:
         hval = hval ^ byte
         hval = (hval * fnv_prime) % fnv_size
-    return hex(hval)
+    return hex(hval)[2:]
 
 
-def fast_hash(data):
-    return fnv1a_64(data)
+def fast_hash(*args, **kwargs):
+    return fnv1a_64(*args, **kwargs)
 
 
-def is_newer(path, other, other_must_exist=False):
-    """Tests if file has been modified more recently than another file"""
-    path_mtime = get_mtime(path)
-    other_mtime = get_mtime(other, other_must_exist)
-    return path_mtime > other_mtime
+def is_newer(path, other, missingok=True):
+    """Tests if file has been modified more recently than another file
 
-
-def is_older(path, other, other_must_exist=False):
-    """Tests if file has been modified less recently than another file"""
-    return not is_newer(other, path, other_must_exist)
+    Returns:
+        True if path is newer or missingok is True and the other file does not exist
+    """
+    mtime1 = os.path.getmtime(path)
+    try:
+        mtime2 = os.path.getmtime(other)
+    except FileNotFoundError:
+        if missingok:
+            return True
+        raise
+    return mtime1 > mtime2
 
 
 def is_different(path, other, compare_image_data=False):
@@ -251,73 +230,14 @@ def is_different(path, other, compare_image_data=False):
     return hash_file(path) != hash_file(other)
 
 
-def get_mtime(fp, must_exist=True):
-    """Gets the modified time for a file"""
-    try:
-        return os.path.getmtime(fp)
-    except FileNotFoundError:
-        if not must_exist:
-            return 0
-        raise
-
-
-def copy_if(src, dst, newer=True, different=True):
-    """Copies source to destination if criteria given in kwargs are met"""
-    assert os.path.isfile(src)
-    # Ensure that dst is a file
-    if "." not in os.path.basename(dst):
-        dst = os.path.join(dst, os.path.basename(src))
-    # Ensure that the destination directory exists
-    try:
-        os.makedirs(os.path.dirname(dst))
-    except OSError:
-        pass
-    # Perform tests
-    try:
-        open(dst, "r")
-    except IOError:
-        copy_file = True
-    else:
-        newer = is_newer(src, dst) if newer else True
-        different = is_different(src, dst) if different else True
-        copy_file = newer and different
-    if copy_file:
-        shutil.copy2(src, dst)
-        return True
-    return False
-
-
-def load_dict(fp, fp_src=None, encoding="utf-8"):
-    """Attempts to load dict from JSON or YML"""
-    if fp_src and is_newer(fp_src, fp):
-        raise IOError("Source file is newer")
-    func = json.load
-    if os.path.splitext(fp)[-1] in {".yaml", ".yml"}:
-        func = yaml.safe_load
-    try:
-        with open(fp, "r", encoding=encoding) as f:
-            result = func(f)
-    except (IOError, OSError, json.JSONDecodeError):
-        raise IOError("Could not load {}".format(fp))
-    else:
-        if result:
-            return result
-        raise IOError("File empty: {}".format(fp))
-
-
 def skip_hashed(f):
     """Skips hashed lines at the beginning of a file"""
-    seek = 0
-    # World's dumbest BOM check
-    first_8 = f.read(8)
-    f.seek(3)
-    if f.read(8) == first_8:
-        seek = 3
-    # Read the file until first unhashed line
-    f.seek(0)
-    for line in f:
-        if not line.strip('"').startswith("#"):
+    size = 0
+    for i, line in enumerate(f):
+        if line.startswith("#") or not i and line[1] == "#":
+            size += len(line)
+        else:
             break
-        seek += len(line)
-    f.seek(seek)
+    f.seek(0)
+    f.read(size)
     return f
---
+++ tests/unit_tests/test_utils_files.py
@@ -0,0 +1,161 @@
+"""Tests the PrefixedNum class"""
+
+import time
+from pathlib import Path
+
+import pytest
+from PIL import Image
+
+from nmnh_ms_tools.utils import (
+    HashCheck,
+    hasher,
+    hash_file,
+    hash_image_data,
+    is_different,
+    is_newer,
+    fast_hash,
+    skip_hashed,
+)
+
+
+@pytest.fixture(scope="session")
+def output_dir(tmpdir_factory):
+    return Path(tmpdir_factory.mktemp("test_utils_files"))
+
+
+@pytest.fixture(scope="session")
+def paths(output_dir):
+    paths = [output_dir / f"{i}.txt" for i in range(1, 4)]
+    with open(paths[0], "w") as f:
+        f.write("abc")
+    time.sleep(0.01)
+    with open(paths[1], "w") as f:
+        f.write("def")
+    time.sleep(0.01)
+    with open(paths[2], "w") as f:
+        f.write("abc")
+    return {
+        paths[0]: "900150983cd24fb0d6963f7d28e17f72",
+        paths[1]: "4ed9407630eb1000c0f6b63842defa7d",
+        paths[2]: "900150983cd24fb0d6963f7d28e17f72",
+    }
+
+
+@pytest.fixture(scope="session")
+def black_image(output_dir):
+    path = output_dir / "black.png"
+    Image.new("RGB", (1, 1), "black").save(path)
+    return path
+
+
+@pytest.fixture(scope="session")
+def white_image(output_dir):
+    path = output_dir / "white.png"
+    Image.new("RGB", (1, 1), "white").save(path)
+    return path
+
+
+@pytest.fixture(scope="function")
+def hashcheck(output_dir):
+    hashcheck = HashCheck()
+    hashcheck.filename = "hashes.json"
+    hashcheck.get_hashes(output_dir)
+    return hashcheck
+
+
+def test_get_path(hashcheck, paths):
+    for path, hash_ in paths.items():
+        assert hashcheck[path][0].hash == hash_
+
+
+def test_get_hash(hashcheck, paths):
+    for path, hash_ in paths.items():
+        assert path in [h.path for h in hashcheck[hash_]]
+
+
+def test_get_name(hashcheck, paths):
+    for path in paths:
+        assert path in [h.path for h in hashcheck[path.name]]
+
+
+def test_iter(hashcheck, paths):
+    for p1, p2 in zip(hashcheck, paths):
+        assert p1 == p2
+
+
+def test_index(hashcheck, paths):
+    assert hashcheck.index == {p.resolve(): h for p, h in paths.items()}
+
+
+def test_hashes(hashcheck, paths):
+    hashes = {}
+    for path, hash_ in paths.items():
+        hashes.setdefault(hash_, []).append(path)
+    assert hashcheck.hashes() == hashes
+
+
+def test_get_duplicates(hashcheck, paths):
+    hash_ = "900150983cd24fb0d6963f7d28e17f72"
+    dupes = hashcheck.get_duplicates()
+    assert dupes[hash_] == [p for p, h in paths.items() if h == hash_]
+
+
+def test_hash_one(hashcheck, paths):
+    for path, hash_ in paths.items():
+        hashcheck.get_hashes(path, overwrite=True)
+        assert hashcheck[path][0].hash == hash_
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [("abc", "e71fa2190541574b"), ("ábc", "b0fd30c6c92a8ad6")],
+)
+def test_fast_hash(test_input, expected):
+    assert fast_hash(test_input, encoding="utf-8") == expected
+
+
+def test_skip_hashed(output_dir):
+    path = output_dir / "skiphashed.txt"
+    with open(path, "w") as f:
+        f.writelines(["# Hashed\n"] * 1000 + ["Not hashed"])
+    with open(path) as f:
+        assert skip_hashed(f).read() == "Not hashed"
+
+
+def test_newer(paths):
+    paths = list(paths)
+    assert is_newer(paths[1], paths[0])
+    assert is_newer(paths[2], paths[1])
+    assert is_newer(paths[0], "fakepath.txt")
+
+
+def test_hash_image(black_image, white_image):
+    assert hash_file(black_image) == "73acd0b4a2391d4bbd9765aca5db19dc"
+    assert hash_image_data(black_image) == "693e9af84d3dfcc71e640e005bdc5e2e"
+    assert hash_file(white_image) == "a1cf09b59e5060f3beccdcf7a37189f0"
+    assert hash_image_data(white_image) == "8597d4e7e65352a302b63e07bc01a7da"
+
+
+def test_is_different(paths):
+    paths = list(paths)
+    assert is_different(paths[0], paths[1])
+    assert is_different(paths[1], paths[2])
+    assert not is_different(paths[0], paths[2])
+
+
+def test_is_different_image(black_image, white_image):
+    assert is_different(black_image, white_image, compare_image_data=True)
+    assert not is_different(black_image, black_image, compare_image_data=True)
+    assert not is_different(white_image, white_image, compare_image_data=True)
+
+
+def test_missing_hash(hashcheck):
+    with pytest.raises(KeyError):
+        hashcheck["abc"]
+
+
+def test_invalid_size(paths):
+    with pytest.raises(ValueError, match="Size must be a multiple of 128"):
+        for path in paths:
+            with open(path, "rb") as f:
+                hasher(f, size=127)

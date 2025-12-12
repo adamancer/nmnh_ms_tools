22260b4c8d107960cbca419ea72f34575fb106da
Adam Mansur <mansura@si.edu>
2025-12-12 14:18:10 -0500

    Do not automatically download untracked files

--- nmnh_ms_tools/config/downloader.py
+++ nmnh_ms_tools/config/downloader.py
@@ -22,49 +22,52 @@ logger = logging.getLogger(__name__)
 
 
 HASHES = {
-    "https://windingway.org/files/nmnh_ms_tools_20201207.zip": "639f0bc4688800571a455f19f44e81d5"
+    "https://windingway.org/files/nmnh_ms_tools_20201207.zip": "f74b9721c13e1d1f31c01ba577392ce0914e4639988d86df4a0ff1b578911e79"
 }
 
 
-def download(url=None):
-    """Downloads untracked data files"""
-    print("FIRST TIME SETUP: Downloading required data files...")
+def download(url=None, sha256hash=None):
+    """Downloads data files that are not included in the repo
+
+    Parameters
+    ----------
+    url : str
+        url of download
+    sha256hash : str
+        expected hash of file content
+    """
     if url is None:
-        url = list(HASHES.keys())[-1]
+        for url in HASHES:
+            download(url)
+    else:
+        print(f"Downloading {url}")
+        response = requests.get(url)
+        if response.status_code == 200:
 
-    response = requests.get(url)
-    if response.status_code == 200:
+            # Verify file against hash
+            resphash = hashlib.sha256(response.content).hexdigest()
+            if sha256hash is None:
+                sha256hash = HASHES[url]
+            if resphash != sha256hash:
+                raise IOError(f"Hash incorrect: {sha256hash}")
 
-        # Verify file against hash
-        if hashlib.md5(response.content).hexdigest() != HASHES[url]:
-            raise IOError("Hash incorrect")
+            # Write content to a temporary file to use with zipfile
+            temp = tempfile.NamedTemporaryFile()
+            temp.write(response.content)
+            temp.seek(0)
 
-        # Write content to a temporary file to use with zipfile
-        temp = tempfile.NamedTemporaryFile()
-        temp.write(response.content)
-        temp.seek(0)
+            # Set and verify output directory
+            output_dir = os.path.join(DATA_DIR, "downloads")
+            try:
+                os.mkdir(output_dir)
+            except OSError:
+                pass
 
-        # Set and verify output directory
-        output_dir = os.path.join(DATA_DIR, "downloads")
-        try:
-            os.mkdir(output_dir)
-        except OSError:
-            pass
-
-        # Extract files to download directory
-        with zipfile.ZipFile(temp) as f:
-            for member in f.namelist():
-                try:
-                    open(os.path.join(output_dir, member))
-                except FileNotFoundError:
-                    f.extract(member, output_dir)
-
-
-# Download non-repo data but allow script to proceed if download fails
-if not os.path.exists(os.path.join(DATA_DIR, "downloads")):
-    try:
-        download()
-    except Exception as exc:
-        msg = f"Could not download {list(HASHES)[-1]}"
-        print(msg)
-        logger.error(msg, exc_info=exc)
+            # Extract files to download directory
+            with zipfile.ZipFile(temp) as f:
+                for member in f.namelist():
+                    try:
+                        open(os.path.join(output_dir, member))
+                    except FileNotFoundError:
+                        print(f" Added {member} to {output_dir}")
+                        f.extract(member, output_dir)
